b9118c65d145371910544fcda224ec551c71703e
Akashgautamkumar <akashgautamkumar@gmail.com>
2025-07-07 09:56:25 +0530

    permutation of all string

---
+++ src/main/java/Recursion/Advance/PermutationString.java
@@ -0,0 +1,25 @@
+package Recursion.Advance;
+
+public class PermutationString {
+    public static void permutation(String str, String permutation){
+       if(str.length()==0){
+           System.out.println(permutation);
+           return;
+       }
+        for(int i=0;i<str.length();i++){
+            char currentchar=str.charAt(i);
+            //abc-> ab
+            String newString=str.substring(0,i)+str.substring(i+1);
+            permutation(newString,permutation+currentchar);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str="abc";
+        permutation(str,"");
+    }
+}
+//permutation
+//-> All possible combination of letter
+//3p3=3!=6
+//n!
