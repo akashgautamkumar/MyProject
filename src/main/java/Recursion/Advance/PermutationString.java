720ad830063e57997db4184bd0f796e75b433a59
akashgautamkumar <akashgautamkumar@gmail.com>
2025-07-12 09:38:58 +0530

    Permutation of String with new technique

--- src/main/java/Recursion/Advance/PermutationString.java
+++ src/main/java/Recursion/Advance/PermutationString.java
@@ -1,22 +1,39 @@
 package Recursion.Advance;
 
 public class PermutationString {
-    public static void permutation(String str, String permutation){
+//    public static void permutation(String str, String permutation){
+//       if(str.length()==0){
+//           System.out.println(permutation);
+//           return;
+//       }
+//        for(int i=0;i<str.length();i++){
+//            char currentchar=str.charAt(i);
+//            //abc-> ab
+//            String newString=str.substring(0,i)+str.substring(i+1);
+//            permutation(newString,permutation+currentchar);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        String str="abc";
+//        permutation(str,"");
+//    }
+    public static void calpemutation(String str, String permutation){
        if(str.length()==0){
            System.out.println(permutation);
            return;
        }
         for(int i=0;i<str.length();i++){
-            char currentchar=str.charAt(i);
-            //abc-> ab
+            char currChar=str.charAt(i);
             String newString=str.substring(0,i)+str.substring(i+1);
-            permutation(newString,permutation+currentchar);
+            calpemutation(newString,permutation+currChar);
         }
+
     }
 
     public static void main(String[] args) {
         String str="abc";
-        permutation(str,"");
+        calpemutation(str,"");
     }
 }
 //permutation
